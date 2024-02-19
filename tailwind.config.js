e53ed216f1bae090324e79cbda4a6d2a8c82dbec
javier Rivera <javier@planetakromo.com>
2024-02-19 16:10:12 +0100

    Basic tailwind structure.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./fvtt-elemental/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: "tw-",
+};
