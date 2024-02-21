fa47d4798707d8bd74c514cb8c7a9898eb777325
javier Rivera <javier@planetakromo.com>
2024-02-21 20:07:49 +0100

    Suppress warnings.

--- fvtt-elemental/src/character-actor.js
+++ fvtt-elemental/src/character-actor.js
@@ -2,11 +2,16 @@
 /* global foundry*/
 
 export class CharacterDataModel extends foundry.abstract.TypeDataModel {
+  // noinspection JSUnusedGlobalSymbols
   static defineSchema() {
     const { fields } = foundry.data;
     return {
       race: new fields.StringField({ initial: "" }),
       title: new fields.StringField({ initial: "" }),
+      agility: new fields.NumberField({integer: true}),
+      toughness: new fields.NumberField({integer: true}),
+      awareness: new fields.NumberField({integer: true}),
+      will: new fields.NumberField({integer: true})
     };
   }
 }
