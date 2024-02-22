92ceffde9f4fd8c674da2393e36ea0cd0681cf07
javier Rivera <javier@planetakromo.com>
2024-02-22 15:06:11 +0100

    Calculate attribute xp cost.

--- fvtt-elemental/src/character-actor.js
+++ fvtt-elemental/src/character-actor.js
@@ -1,5 +1,5 @@
 // Character document and DataModel classes
-/* global foundry*/
+/* global foundry, Actor */
 
 export class CharacterDataModel extends foundry.abstract.TypeDataModel {
   // noinspection JSUnusedGlobalSymbols
@@ -13,5 +13,27 @@ export class CharacterDataModel extends foundry.abstract.TypeDataModel {
       awareness: new fields.NumberField({ integer: true, initial: 0 }),
       will: new fields.NumberField({ integer: true, initial: 0 }),
     };
+  }
+}
+
+export class ElementalActor extends Actor {
+  get agility_xp() {
+    return this.calculate_xp(this.system.agility);
+  }
+
+  get toughness_xp() {
+    return this.calculate_xp(this.system.toughness);
+  }
+
+  get awareness_xp() {
+    return this.calculate_xp(this.system.awareness);
+  }
+
+  get will_xp() {
+    return this.calculate_xp(this.system.will);
+  }
+
+  calculate_xp(value) {
+    return (value * (value + 1)) / 2;
   }
 }
--- fvtt-elemental/src/elemental-main.js
+++ fvtt-elemental/src/elemental-main.js
@@ -2,11 +2,12 @@
 /* globals Actors, ActorSheet, CONFIG, Hooks, game */
 
 import { ElementaCharacterSheet } from "./character-sheet.js";
-import { CharacterDataModel } from "./character-actor.js";
+import { CharacterDataModel, ElementalActor } from "./character-actor.js";
 import { BASE_THEME } from "./theme.js";
 
 Hooks.on("init", () => {
   CONFIG.Actor.systemDataModels.character = CharacterDataModel;
+  CONFIG.Actor.documentClass = ElementalActor;
 
   Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet("elemental", ElementaCharacterSheet, {
