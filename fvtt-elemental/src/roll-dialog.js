848ba57e07ad69665a3228d640e3a5f690a6633d
javier Rivera <javier@planetakromo.com>
2024-04-12 17:05:21 +0200

    Roll stat buttons are working

--- fvtt-elemental/src/roll-dialog.js
+++ fvtt-elemental/src/roll-dialog.js
@@ -1,19 +1,23 @@
 // Classes for all the roll dialogs
-/* globals foundry */
+/* globals foundry, game, FormApplication */
 
 export class StatCheckDialog extends FormApplication {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       template: "systems/fvtt-elemental/templates/stat_check_dialog.hbs",
-      classes: ["elemental-css"],
       closeOnSubmit: true,
       submitOnClose: false,
       submitOnChange: false,
+      width: 250,
     });
   }
 
+  async getData(options) {
+    const data = super.getData(options);
+    return { ...data, theme: game.elemental.current_theme };
+  }
+
   async _updateObject(ev, form_data) {
-    console.log("UPDATE");
-    console.log(ev, form_data);
+    console.log(ev.submitter.value);
   }
 }
--- fvtt-elemental/src/theme.js
+++ fvtt-elemental/src/theme.js
@@ -26,4 +26,6 @@ export const BASE_THEME = {
   skill_button:
     "w-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm",
   skill_name: "border-solid border-0 border-b-2 border-stone-200",
+  roll_button:
+    "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm p-2 m-2",
 };
