977d4d166f1072d64c15a5ece24411e4ef5e0a90
javier Rivera <javier@planetakromo.com>
2024-07-24 17:19:12 +0200

    Move packs to yalm files.

---
+++ utils/unpack.py
@@ -0,0 +1,30 @@
+"""
+A utility to convert LevelDb files to YAML
+"""
+
+import os
+import plyvel
+import yaml
+import json
+from yaml import CDumper as Dumper
+
+PACK_DIST = "../fvtt-elemental/packs"
+PACK_SRC = "../pack_source"
+
+for possible_db in os.listdir(PACK_DIST):
+    print("Scanning,", possible_db)
+    if os.path.isdir(os.path.join(PACK_DIST, possible_db)):
+        if not os.path.exists(os.path.join(PACK_SRC, possible_db)):
+            os.makedirs(os.path.join(PACK_SRC, possible_db))
+        else:
+            for file in os.listdir(os.path.join(PACK_SRC, possible_db)):
+                os.remove(os.path.join(PACK_SRC, possible_db, file))
+        with plyvel.DB(os.path.join(PACK_DIST, possible_db)) as db:
+            for key, value in db:
+                current_object = json.loads(value)
+                name = current_object["name"]
+                if "_stats" in current_object:
+                    del current_object["_stats"]
+                current_object["ownership"] = {"default": 0}
+                with open(os.path.join(PACK_SRC, possible_db, name), 'w') as file:
+                    file.write(yaml.dump(current_object, Dumper=Dumper))
