8ffda19463d37ea0f2d35302a93ea10b1d8f000d
javier Rivera <javier@planetakromo.com>
2024-07-26 15:45:05 +0200

    Try to get back to a nice place in git history after some fun.

--- utils/unpack.py
+++ utils/unpack.py
@@ -3,6 +3,8 @@ A utility to convert LevelDb files to YAML
 """
 
 import os
+import sys
+
 import plyvel
 import yaml
 import json
@@ -33,6 +35,7 @@ def unpack():
             with plyvel.DB(os.path.join(PACK_DIST, possible_db)) as db:
                 for key, value in db:
                     current_object = json.loads(value)
+                    print(key, current_object)
                     name = current_object["name"]
                     clean(current_object)
                     current_object["ownership"] = {"default": 0}
@@ -40,5 +43,24 @@ def unpack():
                         file.write(yaml.dump(current_object, Dumper=Dumper))
 
 
+def pack():
+    for pack_dir in os.listdir(PACK_SRC):
+        if os.path.isdir(os.path.join(PACK_SRC, pack_dir)):
+            print("Find pack", pack_dir)
+            db = plyvel.DB(os.path.join(PACK_DIST, pack_dir), create_if_missing=True)
+            for file in os.listdir(os.path.join(PACK_SRC, pack_dir)):
+                with open(os.path.join(PACK_SRC, pack_dir, file)) as f:
+                    current_object = yaml.load(f, Loader=yaml.Loader)
+                    print(current_object)
+                    clean(current_object)
+                    db.put(
+                        f"!items!{current_object._id}",
+                        json.dumps(current_object).encode()
+                    )
+
+
 if __name__ == "__main__":
-    unpack()
+    if sys.argv[1] == "pack":
+        pack()
+    else:
+        unpack()
