532921429287979b0d05c0c3ca88fc09fd3ef951
javier Rivera <javier@planetakromo.com>
2024-08-28 16:57:52 +0200

    Datamodel for equipment

---
+++ fvtt-elemental/src/equipment-item.js
@@ -0,0 +1,20 @@
+// Equiment datamodel
+/* globals foundry */
+
+export class EquipmentDataModel extends foundry.abstract.TypeDataModel {
+  // noinspection JSUnusedGlobalSymbols
+  static defineSchema() {
+    const { fields } = foundry.data;
+    return {
+      elemental_type: new fields.StringField({ initial: "weapon" }),
+      ammo: new fields.NumberField({ integer: true, initial: 0 }),
+      reload: new fields.NumberField({ integer: true, initial: 0 }),
+      damage: new fields.StringField({ initial: "" }),
+      aoe: new fields.NumberField({ integer: true, initial: 1 }),
+      lbs: new fields.NumberField({ integer: true, initial: 0 }),
+      notes: new fields.StringField({ initial: "" }),
+      description: new fields.HTMLField(),
+      armor: new fields.NumberField({ integer: true, initial: 0 }),
+    };
+  }
+}
