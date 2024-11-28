1af2968ea53596bf7088c4871acdd851029fe18b
javier Rivera <javier@planetakromo.com>
2024-11-28 15:57:13 +0100

    Add a button to modify damage application.

--- fvtt-elemental/src/theme.js
+++ fvtt-elemental/src/theme.js
@@ -31,6 +31,10 @@ export const BASE_THEME = {
   flaw_name: "border-solid border-0 border-b-2 border-stone-200 text-red-700",
   roll_button:
     "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm p-2 m-2",
+  roll_button_left:
+    "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-s-lg text-sm p-2 m-2 mr-0",
+  roll_button_right:
+    "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-e-lg text-sm p-2 m-2 ml-0 border-s-0 w-8",
   result_success:
     "text-green-800 bg-green-50 font-medium rounded-lg text-center mt-2 p-1",
   result_failure:
